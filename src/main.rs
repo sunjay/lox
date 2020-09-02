6b6357aa1c12abf4811fd151f80c65ffb3b6b89b
Sunjay Varma <varma.sunjay@gmail.com>
2020-09-01 21:15:52 -0600

    Setup command line parsing, error handling, and prompt for interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::path::{PathBuf, Path};
+
+use structopt::StructOpt;
+use rustyline::{error::ReadlineError, Editor};
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "lox", about = "An implementation of the lox programming language")]
+struct CmdArgs {
+    /// Input file
+    #[structopt(parse(from_os_str))]
+    input: Option<PathBuf>,
+}
+
+fn main() -> anyhow::Result<()> {
+    let CmdArgs {input} = CmdArgs::from_args();
+
+    match input {
+        Some(input) => run_file(&input),
+        None => run_prompt(),
+    }
+}
+
+fn run_file(input: &Path) -> anyhow::Result<()> {
+    let source_code = fs::read_to_string(input)?;
+    run(source_code)
+}
+
+fn run_prompt() -> anyhow::Result<()> {
+    let mut reader = Editor::<()>::new();
+
+    loop {
+        let readline = reader.readline("> ");
+        match readline {
+            Ok(line) => {
+                reader.add_history_entry(line.as_str());
+                run(line)?;
+            },
+
+            Err(ReadlineError::Interrupted) => {
+                println!("^C");
+                break;
+            },
+
+            Err(ReadlineError::Eof) => {
+                break;
+            },
+
+            Err(err) => Err(err)?,
+        }
+    }
+
+    Ok(())
+}
+
+fn run(source_code: String) -> anyhow::Result<()> {
+    todo!()
 }
