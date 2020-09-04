56b5a3257a1023ece5616bd56722675d8a39fc8e
Sunjay Varma <varma.sunjay@gmail.com>
2020-09-03 20:24:34 -0600

    Refactoring scopes to support arbitrary scope parents

--- src/interpreter/env.rs
+++ src/interpreter/env.rs
@@ -3,12 +3,28 @@ use std::collections::HashMap;
 
 use super::Value;
 
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Clone)]
 pub struct Scope {
+    /// The parent of this scope, represented as an index into `scope_stack`
+    parent: Option<usize>,
     values: HashMap<Arc<str>, Value>,
 }
 
 impl Scope {
+    pub fn global() -> Self {
+        Self {
+            parent: None,
+            values: HashMap::default(),
+        }
+    }
+
+    pub fn new(parent: usize) -> Self {
+        Self {
+            parent: Some(parent),
+            values: HashMap::default(),
+        }
+    }
+
     pub fn insert(&mut self, name: Arc<str>, value: Value) {
         self.values.insert(name, value);
     }
@@ -30,7 +46,7 @@ pub struct Environment {
 impl Default for Environment {
     fn default() -> Self {
         Self {
-            scope_stack: vec![Scope::default()],
+            scope_stack: vec![Scope::global()],
         }
     }
 }
@@ -41,15 +57,42 @@ impl Environment {
     }
 
     pub fn get(&self, name: &str) -> Option<&Value> {
-        self.scope_stack.iter().rev().find_map(|scope| scope.get(name))
+        let mut current_scope = Some(self.scope_stack.len() - 1);
+
+        while let Some(scope_id) = current_scope {
+            let scope = &self.scope_stack[scope_id];
+            match scope.get(name) {
+                Some(value) => return Some(value),
+                None => current_scope = scope.parent,
+            }
+        }
+
+        None
     }
 
-    pub fn get_mut(&mut self, name: &str) -> Option<&mut Value> {
-        self.scope_stack.iter_mut().rev().find_map(|scope| scope.get_mut(name))
+    pub fn get_mut<'a>(&'a mut self, name: &str) -> Option<&'a mut Value> {
+        let mut current_scope = Some(self.scope_stack.len() - 1);
+
+        while let Some(scope_id) = current_scope {
+            let scope = &self.scope_stack[scope_id];
+            match scope.get(name) {
+                Some(_) => break,
+                None => current_scope = scope.parent,
+            }
+        }
+
+        current_scope.and_then(move |scope_id| self.scope_stack[scope_id].get_mut(name))
+    }
+
+    pub fn push_scope_global(&mut self) {
+        // The global scope is always at index zero
+        let parent = 0;
+        self.scope_stack.push(Scope::new(parent));
     }
 
     pub fn push_scope(&mut self) {
-        self.scope_stack.push(Scope::default());
+        let parent = self.scope_stack.len() - 1;
+        self.scope_stack.push(Scope::new(parent));
     }
 
     pub fn pop_scope(&mut self) {
